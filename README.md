0688d6555d393a489f9c3beb103115d7e22fcf3d
Gibirizon <swojteksz@gmail.com>
2024-05-25 22:05:13 +0200

    Requirements, readme update, adding screenshots

--- README.md
+++ README.md
@@ -1,7 +1,61 @@
-﻿# Web application for TCP/IP model
+﻿# Web application about TCP/IP model
 
-This web application is used to educate students about TCP/IP model. It is created with Python as backend.
+This project is a web application developed using Flask, designed to educate students about the TCP/IP model. It provides detailed information on the different layers and protocols of TCP/IP and includes interactive features such as quizzes to assess understanding.
 
-I’m using Python Flask library, which is micro web framework, to handle all of the backend processes. To separate Python logic from HTML I’ve applied Jinja2 – fast, expressive, extensible web templating engine. Special placeholders in the template allow writing code similar to Python syntax. For styling in css I’ve implemented Bootstrap frontend framework, but I’ve modified a lot of styles and added my own layout and appearance.
+## Features
 
-This app includes home page, authentication page, some informations about layers and protocols, and quiz page where you can work on your skills and add your own questions. 
+- **Educational Content**: Detailed information about each layer of the TCP/IP model and their associated protocols.
+- **User Authentication**: Secure user login and registration.
+- **Interactive Quizzes**: Test your knowledge with quizzes on TCP/IP concepts.
+- **Responsive Design**: Frontend styled with Bootstrap for a responsive and modern interface.
+
+## Screenshots
+
+![Home Page](screenshots/home_page.png)
+![Quiz First Question](screenshots/quiz.png)
+![Network Access Layer](screenshots/layer_example.png)
+![Register page](screenshots/registering.png)
+
+## Installation
+
+To set up the project on your local machine, follow these steps:
+
+1. **Clone the Repository**:
+
+   ```sh
+   git clone https://github.com/Gibirizon/web-app-tcp-ip.git
+   cd web-app-tcp-ip
+   ```
+
+2. **Create a Virtual Environment**:
+
+   ```sh
+   python3 -m venv venv
+   source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+   ```
+
+3. **Install Dependencies**:
+
+   ```sh
+   pip install -r requirements.txt
+   ```
+
+4. **Run the Application**:
+
+   ```sh
+   flask --app main run
+   ```
+
+5. **Access the Application**:
+   Open your web browser and navigate to `http://127.0.0.1:5000/home`.
+
+## Usage
+
+- **Home Page**: Overview of the TCP/IP model.
+- **User Authentication**: Register and log in to access quizzes.
+- **TCP/IP Information**: Navigate through detailed pages about each layer and protocol.
+- **Quizzes**: Test your knowledge on the TCP/IP model.
+
+---
+
+Made by Gibirizon :heart:
--- website/quiz/quiz.py
+++ website/quiz/quiz.py
@@ -13,8 +13,7 @@ from flask_login import current_user, login_required
 from .. import db
 from ..models import Answer, Question
 
-quiz = Blueprint("quiz", __name__, static_folder="static",
-                 template_folder="templates")
+quiz = Blueprint("quiz", __name__, static_folder="static", template_folder="templates")
 
 
 @quiz.route("/", methods=["GET", "POST"])
@@ -23,16 +22,20 @@ quiz = Blueprint("quiz", __name__, static_folder="static",
 def quiz_home():
     if request.method == "POST":
         if request.form.get("link") == "rozpocznij-quiz":
-            obj = [i.to_dict() for i in Question.query.filter_by(
-                user_id=current_user.id).all()]
+            obj = [
+                i.to_dict()
+                for i in Question.query.filter_by(user_id=current_user.id).all()
+            ]
             session["questions"] = obj
             session["question_amount"] = len(obj)
             session["question_number"] = 1
             session["correct_answers"] = 0
             return redirect(url_for(".quiz_start"))
         else:
-            obj = [i.to_dict() for i in Question.query.filter_by(
-                user_id=current_user.id).all()]
+            obj = [
+                i.to_dict()
+                for i in Question.query.filter_by(user_id=current_user.id).all()
+            ]
             session["current_user_questions"] = obj
             return redirect(url_for(".quiz_add"))
     return render_template("quiz_home.html", user=current_user)
@@ -42,50 +45,47 @@ def quiz_home():
 @login_required
 def quiz_add():
 
-    if request.method == "POST" and Question.query.filter_by(question=request.data.decode()).first():
-        print("good")
-        question = Question.query.filter_by(
-            question=request.data.decode()).first()
-        print(question.id)
+    if (
+        request.method == "POST"
+        and Question.query.filter_by(question=request.data.decode()).first()
+    ):
+        question = Question.query.filter_by(question=request.data.decode()).first()
         answers = Answer.query.filter_by(question_id=question.id).first()
         db.session.delete(answers)
         db.session.delete(question)
         db.session.commit()
-        obj = [i.to_dict() for i in Question.query.filter_by(
-            user_id=current_user.id).all()]
-        print(obj)
+        obj = [
+            i.to_dict() for i in Question.query.filter_by(user_id=current_user.id).all()
+        ]
         session["current_user_questions"] = obj
     elif request.method == "POST":
-        print("sth")
         question = request.form.get("question")
         answer1 = request.form.get("answer1")
         answer2 = request.form.get("answer2")
         answer3 = request.form.get("answer3")
         answer4 = request.form.get("answer4")
-        print(question, answer1)
         correct_answer = request.form.get("correct_answer")
-        print(session["current_user_questions"])
         if Question.query.filter_by(question=question).first():
 
             flash("Już istnieje takie pytanie", category="info")
             return redirect(url_for(".quiz_add"))
 
-        conditions = {"Treść pytania - to miejsce jest wymagane": question,
-                      "Odpowiedź 1 - to miejsce jest wymagane": answer1,
-                      "Odpowiedź 2 - to miejsce jest wymagane": answer2,
-                      "Odpowiedź 3 - to miejsce jest wymagane": answer3,
-                      "Odpowiedź 4 - to miejsce jest wymagane": answer4,
-                      "Poprawna odpowiedź - to miejsce jest wymagane": correct_answer,
-                      "Jedna z podanych odpowiedzi musi być poprawna": correct_answer in [answer1, answer2, answer3, answer4], }
+        conditions = {
+            "Treść pytania - to miejsce jest wymagane": question,
+            "Odpowiedź 1 - to miejsce jest wymagane": answer1,
+            "Odpowiedź 2 - to miejsce jest wymagane": answer2,
+            "Odpowiedź 3 - to miejsce jest wymagane": answer3,
+            "Odpowiedź 4 - to miejsce jest wymagane": answer4,
+            "Poprawna odpowiedź - to miejsce jest wymagane": correct_answer,
+            "Jedna z podanych odpowiedzi musi być poprawna": correct_answer
+            in [answer1, answer2, answer3, answer4],
+        }
         for message, condition in conditions.items():
             if not condition:
                 flash(message, category="error")
 
         if all(conditions.values()):
-            question = Question(
-                question=question,
-                user_id=current_user.id
-            )
+            question = Question(question=question, user_id=current_user.id)
             db.session.add(question)
             db.session.commit()
             answer = Answer(
@@ -94,14 +94,15 @@ def quiz_add():
                 answer3=answer3,
                 answer4=answer4,
                 correct_answer=correct_answer,
-                question_id=question.id
-
+                question_id=question.id,
             )
             db.session.add(answer)
             db.session.commit()
 
-            obj = [i.to_dict() for i in Question.query.filter_by(
-                user_id=current_user.id).all()]
+            obj = [
+                i.to_dict()
+                for i in Question.query.filter_by(user_id=current_user.id).all()
+            ]
             session["current_user_questions"] = obj
 
             flash("Dodano pytanie", category="success")
@@ -113,8 +114,9 @@ def quiz_add():
 def quiz_start():
     if request.method == "POST":
         answer = request.form.get("answer")
-        session["correct_answer"] = session["questions"][session["question_number"] -
-                                                         1]["answers"]["correct_answer"]
+        session["correct_answer"] = session["questions"][
+            session["question_number"] - 1
+        ]["answers"]["correct_answer"]
         if answer == str(session["correct_answer"]):
             flash("Poprawna odpowiedź", category="success")
             session["correct_answers"] += 1
@@ -124,22 +126,30 @@ def quiz_start():
             return redirect(url_for(".quiz_score"))
         session["question_number"] += 1
 
-    session["question"] = session["questions"][session["question_number"]-1]["question"]
-    ans = session["questions"][session["question_number"]-1]["answers"]
-    session["answers"] = [ans["answer1"],
-                          ans["answer2"], ans["answer3"], ans["answer4"]]
+    session["question"] = session["questions"][session["question_number"] - 1][
+        "question"
+    ]
+    ans = session["questions"][session["question_number"] - 1]["answers"]
+    session["answers"] = [
+        ans["answer1"],
+        ans["answer2"],
+        ans["answer3"],
+        ans["answer4"],
+    ]
 
     return render_template("quiz_start.html", user=current_user)
 
 
 @quiz.route("/wynik/", methods=["GET", "POST"])
 def quiz_score():
-    if request.method == 'POST':
+    if request.method == "POST":
         if request.form.get("link") == "wiedza":
             return redirect(url_for("layers.app_layer"))
         else:
-            obj = [i.to_dict() for i in Question.query.filter_by(
-                user_id=current_user.id).all()]
+            obj = [
+                i.to_dict()
+                for i in Question.query.filter_by(user_id=current_user.id).all()
+            ]
             session["current_user_questions"] = obj
             return redirect(url_for(".quiz_add"))
     return render_template("quiz_score.html", user=current_user)
